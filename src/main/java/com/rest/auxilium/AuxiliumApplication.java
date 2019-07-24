d55395610e3836649e98f2888343c3a5ae8af2ca
aawoznia2644_do_github <ania.woznia@gmail.com_do_github>
2019-07-24 20:51:58 +0200

    first commit

---
+++ src/main/java/com/rest/auxilium/AuxiliumApplication.java
@@ -0,0 +1,13 @@
+package com.rest.auxilium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuxiliumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuxiliumApplication.class, args);
+	}
+
+}
